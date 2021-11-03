77f1bfd514fb5a3359626bef6d3cfbed3cbe3d9c
zhucaiyun <zhucaiyun1@xdf.cn>
2021-11-03 18:38:31 +0800

    feat:提交第一版 @vue/cli-service-global

---
+++ app.vue
@@ -0,0 +1,13 @@
+<!--
+ * @Author       : zhucaiyun1@xdf.cn
+ * @Date         : 2021-11-03 18:28:55
+ * @LastEditors  : zhucaiyun1@xdf.cn
+ * @LastEditTime : 2021-11-03 18:30:14
+ * @Description  : 描述信息
+-->
+<template>
+  <div>
+    两个入口 有index.js 以index.js为入口 否则再看有没有app.vue
+  </div>
+</template>
+<script src="./test.js"> </script>
---
+++ test.js
@@ -0,0 +1,11 @@
+/*
+ * @Author       : zhucaiyun1@xdf.cn
+ * @Date         : 2021-11-03 17:18:11
+ * @LastEditors  : zhucaiyun1@xdf.cn
+ * @LastEditTime : 2021-11-03 18:27:33
+ * @Description  : 测试快速原型开发
+ */
+console.log('@vue/cli-service-global')
+
+console.log('vue serve')
+// 必须以app.vue为入口Valid entry file should be one of: main.js, index.js, App.vue or app.vue.
